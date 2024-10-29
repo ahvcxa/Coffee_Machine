a7e303d9fc7a85a682e36e471f66913a7dea21ec
BATUHAN INAN <66177097+ahvcxa@users.noreply.github.com>
2024-10-30 00:40:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from menu import Menu
-from coffee_maker import CoffeeMarker
+from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 will_continue = True
 menu = Menu()
 name = menu.get_items()
 money_machine = MoneyMachine()
-coffee_marker = CoffeeMarker()
+coffee_maker = CoffeeMaker()
 
 while will_continue:
-    choice = input(f'{name}\n\nWhat would you like: ').lower()
-    if choice == 'off':
-        will_continue = False
-    elif choice == 'report':
-        coffee_marker.report()
-        money_machine.report()
-    else:
+    try:
+        choice = input(f'{name}\n\nWhat would you like: ').lower()
 
-        drink = menu.find_drink(choice)
-        if coffee_marker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_marker.make_coffee(drink)
+        if choice == 'off':
+            will_continue = False
+        elif choice == 'report':
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(choice)
+
+            if drink is None:
+                print("\n🔎Sorry, that item is not on the menu.🔎\n\n")
+                continue
+
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+    except ValueError as ve:
+        print(f"\n💲📥💲Please insert a valid money💲📥💲\n\n")
